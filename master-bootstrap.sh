fff5fdcfe965b6cfc4a4ca2e549059a625390bec
Ali Almohammad <ali@iotechsys.com>
2021-03-22 17:45:59 +0000

    SYS-950 - Added master and node VMs

---
+++ master-bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Install ifconfig  
+apt-get install -y net-tools
+
+# Install docker
+apt-get install -y docker.io
+usermod -aG docker vagrant
+
+# Install docker-compose
+apt-get install -y docker-compose
+
+sudo apt-get update
+
+# Install edgebuilder server components
+apt-get install -y /vagrant/edgebuilder-server-component/PACKAGE/ubuntu-20.04/edgebuilder-server-2.0.0_amd64.deb
+
+# Install edgebuilder cli
+apt-get install -y /vagrant/edgebuilder-cli/PACKAGE/edgebuilder-cli-2.0.0_amd64.deb
+
---
+++ node-bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Install ifconfig  
+apt-get install -y net-tools
+
+# Install docker
+apt-get install -y docker.io
+usermod -aG docker vagrant
+
+# Install docker-compose
+apt-get install -y docker-compose
+
+# Required to install SaltStack
+wget -O - https://repo.saltstack.com/py3/ubuntu/20.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+echo "deb http://repo.saltstack.com/py3/ubuntu/20.04/amd64/latest focal main" | sudo tee -a /etc/apt/sources.list.d/saltstack.list
+sudo apt-get update
+
+# Install edgebuilder node components
+apt-get install -y /vagrant/edgebuilder-node-components/PACKAGE/ubuntu-20.04/edgebuilder-node-2.0.0_amd64.deb
+
