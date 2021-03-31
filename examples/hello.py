d407fd4559c5314d0d9ff13505c3fb9fa5472dcb
Ali Almohammad <ali@iotechsys.com>
2021-03-31 11:37:44 +0100

    SYS-953 - Added examples for script definition

---
+++ examples/hello.py
@@ -0,0 +1,3 @@
+print("Hello World!")
+print("changed=yes")
+exit(0)
---
+++ examples/long.py
@@ -0,0 +1,8 @@
+import time
+
+print("Long script - start")
+time.sleep(300) # Sleep for 5 minutes
+print("Long script - end")
+
+print("changed=yes")
+exit(0)
