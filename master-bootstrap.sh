9b2ffaa70bb8b7414eab407e7b98f3b44051dbcf
rakesh <rakesh@iotechsys.com>
2021-06-03 14:34:23 +0100

    SYS-1233 : Updates for v0.9.1 - removing the docker and docker-compose from bootstrap

--- master-bootstrap.sh
+++ master-bootstrap.sh
@@ -4,18 +4,13 @@ export DEBIAN_FRONTEND=noninteractive
 
 # Install ifconfig  
 apt-get install -y net-tools
-
-# Install docker
-apt-get install -y docker.io
-usermod -aG docker vagrant
-
-# Install docker-compose
-apt-get install -y docker-compose
-
 sudo apt-get update
 
 # Install edgebuilder server components
 bash ./vagrant/edgebuilder-install.sh server
+
+# Add vagrant to docker group
+usermod -aG docker vagrant
 
 # Install edgebuilder cli
 bash ./vagrant/edgebuilder-install.sh cli
--- node-bootstrap.sh
+++ node-bootstrap.sh
@@ -5,13 +5,6 @@ export DEBIAN_FRONTEND=noninteractive
 # Install ifconfig  
 apt-get install -y net-tools
 
-# Install docker
-apt-get install -y docker.io
-usermod -aG docker vagrant
-
-# Install docker-compose
-apt-get install -y docker-compose
-
 # Required to install SaltStack
 wget -O - https://repo.saltstack.com/py3/ubuntu/20.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
 echo "deb http://repo.saltstack.com/py3/ubuntu/20.04/amd64/latest focal main" | sudo tee -a /etc/apt/sources.list.d/saltstack.list
@@ -19,6 +12,9 @@ sudo apt-get update
 
 # Install edgebuilder node components
 bash ./vagrant/edgebuilder-install.sh server
+
+# Add vagrant to docker group
+usermod -aG docker vagrant
 
 # Required to run a simple webserver for App Definition
 mkdir /home/vagrant/src
