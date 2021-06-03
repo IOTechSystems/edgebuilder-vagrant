254967577781de8568fdf7119ad1e32b5a91671e
rakesh <rakesh@iotechsys.com>
2021-06-03 16:48:46 +0100

    SYS-1233 : Updates for v0.9.1 - Fixing the box version

--- master-bootstrap.sh
+++ master-bootstrap.sh
@@ -4,14 +4,14 @@ export DEBIAN_FRONTEND=noninteractive
 
 # Install ifconfig  
 apt-get install -y net-tools
-sudo apt-get update
+apt-get update
 
 # Install edgebuilder server components
-bash ./vagrant/edgebuilder-install.sh server
+. /vagrant/edgebuilder-install.sh server
 
 # Add vagrant to docker group
 usermod -aG docker vagrant
 
 # Install edgebuilder cli
-bash ./vagrant/edgebuilder-install.sh cli
+. /vagrant/edgebuilder-install.sh cli
 
--- node-bootstrap.sh
+++ node-bootstrap.sh
@@ -6,12 +6,12 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get install -y net-tools
 
 # Required to install SaltStack
-wget -O - https://repo.saltstack.com/py3/ubuntu/20.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+wget -q -O - https://repo.saltstack.com/py3/ubuntu/20.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
 echo "deb http://repo.saltstack.com/py3/ubuntu/20.04/amd64/latest focal main" | sudo tee -a /etc/apt/sources.list.d/saltstack.list
-sudo apt-get update
+apt-get update
 
 # Install edgebuilder node components
-bash ./vagrant/edgebuilder-install.sh server
+. /vagrant/edgebuilder-install.sh node
 
 # Add vagrant to docker group
 usermod -aG docker vagrant
