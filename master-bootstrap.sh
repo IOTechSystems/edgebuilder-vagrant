b75b8e894149df22221af6eaa2425100a7911e68
rakesh <rakesh@iotechsys.com>
2021-06-04 13:44:15 +0100

    SYS-1233 : Updates for v0.9.1 - Automating Edge Builder install

--- master-bootstrap.sh
+++ master-bootstrap.sh
@@ -7,11 +7,12 @@ apt-get install -y net-tools
 apt-get update
 
 # Install edgebuilder server components
-. /vagrant/edgebuilder-install.sh server
+wget -q -O - https://github.com/IOTechSystems/edgebuilder-installer/archive/refs/tags/v0.9.1.tar.gz | tar xvz -C /vagrant/
+. /vagrant/edgebuilder-installer-0.9.1/edgebuilder-install.sh server
 
 # Add vagrant to docker group
 usermod -aG docker vagrant
 
 # Install edgebuilder cli
-. /vagrant/edgebuilder-install.sh cli
+. /vagrant/edgebuilder-installer-0.9.1/edgebuilder-install.sh cli
 
--- node-bootstrap.sh
+++ node-bootstrap.sh
@@ -6,12 +6,13 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get install -y net-tools
 
 # Required to install SaltStack
-wget -q -O - https://repo.saltstack.com/py3/ubuntu/20.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+wget -q -O - https://repo.saltstack.com/py3/ubuntu/20.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add >/dev/null 2>&1
 echo "deb http://repo.saltstack.com/py3/ubuntu/20.04/amd64/latest focal main" | sudo tee -a /etc/apt/sources.list.d/saltstack.list
 apt-get update
 
 # Install edgebuilder node components
-. /vagrant/edgebuilder-install.sh node
+wget -q -O - https://github.com/IOTechSystems/edgebuilder-installer/archive/refs/tags/v0.9.1.tar.gz | tar xvz -C /vagrant/
+. /vagrant/edgebuilder-installer-0.9.1/edgebuilder-install.sh node
 
 # Add vagrant to docker group
 usermod -aG docker vagrant
