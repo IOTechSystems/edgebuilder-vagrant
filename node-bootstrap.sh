1359a7fa4ebae2a99edc4792071a733a1273ddc7
Tom Fleming <tom@iotechsys.com>
2021-12-14 09:31:42 +0000

    SYS-1841 updated node bootstrap script
    Signed-off-by: Tom Fleming <tom@iotechsys.com>

--- node-bootstrap.sh
+++ node-bootstrap.sh
@@ -6,11 +6,6 @@ export DEBIAN_FRONTEND=noninteractive
 if [ "$CREDS" != "" ]; then
   CREDS="-r $CREDS"
 fi
-# Install ifconfig
-apt-get install -y net-tools
-
-# Install edgebuilder server components
-sudo sh -c 'wget -O - https://raw.githubusercontent.com/IOTechSystems/edgebuilder-installer/'"$VER"'/edgebuilder-install.sh | sh -s - node '"$CREDS"
 
 # Required to run a simple webserver for App Definition
 mkdir /home/vagrant/src
